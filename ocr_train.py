95b4c6f2c37a73f9127589d7a97a2cbd4675ab20
01Jonathan10 <jonathansuper1@gmail.com>
2020-11-19 00:21:05 -0300

    ajuste path

--- ocr_train.py
+++ ocr_train.py
@@ -20,7 +20,7 @@ labels = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 categories = len(labels)
 batch_size = 1024
 epochs = 50
-model_path = '/home/seaquest/Arthur/ic/text-recog/folder/modelo'
+model_path = 'folder/modelo'
 
 #codigo copiado p/ abrir os index do emnist
 def read_idx(filename):
