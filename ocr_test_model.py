93a7826af1caab01a8ae37cee246e1060a9c5c8a
01Jonathan10 <jonathansuper1@gmail.com>
2020-11-20 00:25:44 -0300

    test

--- ocr_test_model.py
+++ ocr_test_model.py
@@ -9,7 +9,9 @@ from PIL import Image
 import PIL.ImageOps
 
 model_path = 'test/modelo'
-test_path = 'test/aprint.png'
+test_path = 'test/p.png'
+# test_path = 'test/modelprint.png'
+# test_path = 'test/aprint.png'
 test_path2 = 'new_file.png'
 model = tensorflow.keras.models.load_model(model_path)
 
@@ -31,24 +33,8 @@ else:
     inverted_image = PIL.ImageOps.invert(image)
     inverted_image.save('new_name.png')
 
-# #ploting o modelo
-# plt.figure(figsize=(12, 6), dpi=96)
-# plt.subplot(1, 2, 1)
-# plt.plot(fit.history['loss'])
-# plt.plot(fit.history['val_loss']) 
-# plt.title('Model Loss')
-# plt.xlabel('Epoch')
-# plt.ylabel('Loss')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.subplot(1, 2, 2)
-# plt.plot(fit.history['accuracy'])
-# plt.plot(fit.history['val_accuracy'])
-# plt.title('Model Accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.tight_layout()
-# plt.show()
+model = tensorflow.keras.models.load_model(model_path)
+
 labels = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
 test_image = cv2.imread(test_path)
@@ -60,18 +46,23 @@ test_image = test_image.reshape(test_image.shape + (1,))
 y_pred = model.predict(np.array([test_image]))
 predictions = y_pred[0]
 result = np.argmax(predictions)
-print("Class: ",result)
-print(model.summary())
-
-testtt = test_path2
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(labels[np.argmax(predictions)], 100 * np.max(predictions))
+)
 
 img = keras.preprocessing.image.load_img(
-    testtt, target_size=(28, 28) ,color_mode="grayscale"
+    test_path, target_size=(28, 28), color_mode='grayscale'
 )       # 107 98 target_size=(img_height, img_width)
 img_array = keras.preprocessing.image.img_to_array(img)
 img_array = tensorflow.expand_dims(img_array, 0) # Create a batch
-print ( img_array )
-predictions = model.predict(img_array)
+img_array = np.array(img_array[0]).astype('float32')
+img_array = img_array / 255
+
+cv2.imshow("Window", img_array)
+cv2.waitKey(0)
+
+predictions = model.predict(np.array([img_array]))
 score = tensorflow.nn.softmax(predictions[0])
 print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
